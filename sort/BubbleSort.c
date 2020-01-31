1416849151805f10719b615942771b9b3e259cbf
letitride <ichikawa.fumiya@gmail.com>
2020-01-31 15:11:41 +0900

    バブルソート改良

--- sort/BubbleSort.c
+++ sort/BubbleSort.c
@@ -26,6 +26,24 @@ void BubbleSort(void){
     }while(flag==1);
 }
 
+//バブルソート改良　ループ1回につき末尾-iは必ず配列内の最大値になっている
+void ExBubbleSort(void){
+    int i, j, flag, k;
+    k = 0;
+    do{
+        flag = 0;
+        for(i=0;i<N-1-k;i++){
+            if( sort[i] > sort[i+1] ){
+                flag = 1;
+                j = sort[i];
+                sort[i] = sort[i+1];
+                sort[i+1] = j;
+            }
+        }
+        k++;
+    }while(flag==1);
+}
+
 int main(void){
     int i;
     srand( (unsigned int)time(NULL) );
