a5d6610f5cbe21ba22cc5c0489bd1f48ba8635fb
letitride <ichikawa.fumiya@gmail.com>
2020-02-06 08:47:02 +0900

    binary treeの実装サンプル

---
+++ tree/binary_tree.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _tag_tree_node {
+  int value;
+  struct _tag_tree_node *left;
+  struct _tag_tree_node *right;
+} tree_node;
+
+tree_node *tree_root = NULL;
+
+tree_node* create_new_node(int num){
+  tree_node *tree_new;
+  tree_new = (tree_node*)malloc( sizeof(tree_node) );
+  if( tree_new == NULL ){
+      exit(EXIT_FAILURE);
+  }
+  tree_new->left = NULL;
+  tree_new->right = NULL;
+  tree_new->value = num;
+  return tree_new;
+}
+
+void insert_tree(int num, tree_node *node){
+  if( node == NULL ){
+    tree_root = create_new_node(num);
+    return;
+  }
+  if( node->value > num ){
+    if( node->left != NULL ){
+      insert_tree( num, node->left );
+    }
+    else{
+      node->left = create_new_node(num);
+    }
+  }
+  else{
+    if( node->right != NULL ){
+      insert_tree( num, node->right );
+    }
+    else{
+      node->right = create_new_node(num);
+    }
+  }
+  return;
+}
+
+int main(void){
+  insert_tree(5, tree_root);
+  insert_tree(3, tree_root);
+  insert_tree(1, tree_root);
+  insert_tree(4, tree_root);
+  insert_tree(8, tree_root);
+
+  printf("root: %d \n", tree_root->value  );
+  printf("root->left: %d \n", tree_root->left->value  );
+  printf("root->left->left: %d \n", tree_root->left->left->value  );
+  printf("root->left->right: %d \n", tree_root->left->right->value  );
+  printf("root->right: %d \n", tree_root->right->value  );
+
+  return EXIT_SUCCESS;
+}
