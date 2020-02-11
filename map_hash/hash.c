40b3ad9cfdae89bb1567155ab9ed0a86f01dcb0c
letitride <ichikawa.fumiya@gmail.com>
2020-02-11 09:28:48 +0900

    ハッシュマップに辞書データを追加

--- map_hash/hash.c
+++ map_hash/hash.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+//単語セット
+typedef struct{
+    char *english;
+    char *japanese;
+} WORDSET;
+
+//ハッシュ表
+typedef struct{
+    WORDSET **data;
+    unsigned int size;
+} HASHTABLE;
 
 unsigned int MakeHash(char *str, unsigned int hashmax){
     unsigned int n, length, hash, weight;
@@ -19,9 +32,9 @@ unsigned int ReHash(HASHTABLE *hashtable, unsigned int firsthash){
     unsigned int hashval, k;
     
     for(k=1;k<=hashtable->size/2;k++){
-        //再ハッシュ値は元の数にloop^2を足す
+        //再ハッシュ値は元の数にloop倍率を掛け合わせる
         hashval = (firsthash+k*k) % hashtable->size;
-        //新しいハッシュがハッシュ表で衝突がなければ値を返す
+        //ハッシュ表に衝突がなければ値を返す
         if( hashtable->data[hashval] == NULL ){
             return hashval;
         }
@@ -29,8 +42,27 @@ unsigned int ReHash(HASHTABLE *hashtable, unsigned int firsthash){
     return -1;
 }
 
+void AddDataToMap(HASHTABLE *hashtable, WORDSET *newdata){
+    unsigned int hashval;
+    hashval = MakeHash(newdata->english, hashtable->size);
+    if( hashtable->data[hashval] != NULL ){
+        hashval = ReHash(hashtable, hashval);
+        if( hashval == -1 ){
+            printf("%sをマップに挿入しようとしましたが空き位置がありませんでした\n", newdata->english);
+            return;
+        }
+    }
+    hashtable->data[hashval] = newdata;
+}
+
 int main(void){
-    //ハッシュサイズは大きい素数が良い
-    printf("%d", MakeHash("abcd", 10007));
+    unsigned int n;
+    char key[64], *japanese;
+    HASHTABLE hashtable;
+    WORDSET *wordfound;
+    WORDSET words[5] = {
+        {"dolphin", "イルカ"},{"beluga","シロイルカ"},{"prampus","シャチ"},
+        {"medusa", "海月"}, {"otter", "カワウソ"}
+    };
     return EXIT_SUCCESS;
 }
